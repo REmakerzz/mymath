6340d56fcad40289bfd3b5994a2df8cec9e94369
Dylyk Oidopov <9dylyk@gmail.com>
2024-10-09 12:11:03 +0900

    create myPackage

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/REmakerzz/mymath
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,21 @@
+package mymath
+
+import (
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Max(x float64, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
